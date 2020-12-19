542324b27d9a4840894a8bc8dd5600dc78cd3a22
Gustavo Maia Paes <gump.flash@gmail.com>
2020-12-19 16:09:57 -0300

    certificates added

--- classes/Stream.ts
+++ classes/Stream.ts
@@ -1,9 +1,16 @@
+import { readFileSync } from "fs";
+import https from 'https';
 export default class Stream{
     public static io : any;
     public static serverHandles : any = {};
     
     constructor(port){
-        const io = require("socket.io")(port, {
+        const options = {
+            key: readFileSync('./certs/CA-key.pem').toString(),
+            cert: readFileSync('./certs/CA-cert.pem').toString()
+        }
+        https.createServer(options).listen(port);
+        const io = require("socket.io")(https, {
             cors: {
               origin: '*',
             }
